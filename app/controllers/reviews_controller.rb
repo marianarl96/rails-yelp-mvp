590d2f611de611048af2998168ff4f5e28d90ff6
mariana regueira <regueiram7@gmail.com>
2021-10-08 09:04:00 -0500

    Added Models, Controllers and a Few Views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,8 @@
 class ReviewsController < ApplicationController
+  def new
+    @new = Review.new
+  end
+
+  def create
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: %w[chinese italian japanese french belgian].sample)
+  Review.create(
+    rating: rand(1..5),
+    content: Faker::Restaurant.review,
+    restaurant: restaurant)
+end
